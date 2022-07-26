4d1668e55bda41c5ece79cda7ba9a6ca5247d554
Manish Negi <manishng3@gmail.com>
2022-07-26 19:50:09 +0530

    -replaced normal layout usage to databinding

--- app/src/main/java/com/manish/androiddevcourse/MainActivity.kt
+++ app/src/main/java/com/manish/androiddevcourse/MainActivity.kt
@@ -2,10 +2,19 @@ package com.manish.androiddevcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.manish.androiddevcourse.data.Student
+import com.manish.androiddevcourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityDataBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainActivityDataBinding.student = getStudent()
+    }
+
+    private fun getStudent(): Student {
+        return Student(1,"Manish","manishng3@gmail.com")
     }
 }
