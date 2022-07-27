0a26213cd2f61a293dbbfd04fd073dbe2b1f5bca
Manish Negi <manishng3@gmail.com>
2022-07-27 19:02:09 +0530

    -added basic VM code

--- app/src/main/java/com/manish/androiddevcourse/MainActivityViewModel.kt
+++ app/src/main/java/com/manish/androiddevcourse/MainActivityViewModel.kt
@@ -3,5 +3,13 @@ package com.manish.androiddevcourse
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
+    private var count = 0
 
+    fun getCurrentCount() : Int {
+        return count
+    }
+
+    fun getUpdatedCount() : Int {
+        return ++count
+    }
 }
