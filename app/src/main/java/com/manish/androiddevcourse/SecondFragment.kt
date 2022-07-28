aa1eb7b04a674a6e231c510c70ae77e765ee027b
Manish Negi <manishng3@gmail.com>
2022-07-28 20:04:45 +0530

    -Passed data from home to second destination using bundles

--- app/src/main/java/com/manish/androiddevcourse/SecondFragment.kt
+++ app/src/main/java/com/manish/androiddevcourse/SecondFragment.kt
@@ -17,6 +17,8 @@ class SecondFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        val input = arguments?.getString("user_input")
+        binding.secondFragTextView.text = input
         return binding.root
     }
 
