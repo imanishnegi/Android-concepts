519a9792df0cd0bcfaf5c097c76aaaec767b6d0b
Manish Negi <manishng3@gmail.com>
2022-07-28 19:47:20 +0530

    -Added a second destination

--- app/src/main/java/com/manish/androiddevcourse/SecondFragment.kt
+++ app/src/main/java/com/manish/androiddevcourse/SecondFragment.kt
@@ -7,16 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.manish.androiddevcourse.databinding.FragmentHomeBinding
+import com.manish.androiddevcourse.databinding.FragmentSecondBinding
 
-class HomeFragment : Fragment() {
-
-    private lateinit var binding : FragmentHomeBinding
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
         return binding.root
     }
 
