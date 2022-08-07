1832b7e720a1d9a5dd9febc11a5c0fdbe724ce78
Manish Negi <manishng3@gmail.com>
2022-08-07 12:13:02 +0530

    - added input data to send to the worked class - getting the output received from the worker class

--- app/src/main/java/com/manish/androiddevcourse/UploadWorker.kt
+++ app/src/main/java/com/manish/androiddevcourse/UploadWorker.kt
@@ -2,21 +2,36 @@ package com.manish.androiddevcourse
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * This is the worker class containing the work description
  */
 class UploadWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
+    companion object {
+        const val KEY_OUTPUT_DATA = "output_data"
+    }
+
     override fun doWork(): Result {
+        val inputData = inputData.getInt(WorkManagerActivity.KEY_INPUT_DATA, 0)
         return try {
-            for (i in 1..600000) {
+            for (i in 1 until inputData) {
                 Log.i("MYTAG", "doWork: $i")
             }
-            Result.success()
+
+            //creating output data to send with the work manager result
+            val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val date = time.format(Date())
+            val outputData = Data.Builder()
+                .putString(KEY_OUTPUT_DATA, date)
+                .build()
+            Result.success(outputData)
         } catch (e: Exception) {
             Result.failure()
         }
--- app/src/main/java/com/manish/androiddevcourse/WorkManagerActivity.kt
+++ app/src/main/java/com/manish/androiddevcourse/WorkManagerActivity.kt
@@ -3,13 +3,19 @@ package com.manish.androiddevcourse
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.work.Constraints
+import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
 class WorkManagerActivity : AppCompatActivity() {
+    companion object {
+        const val KEY_INPUT_DATA = "input_data"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_manager)
@@ -25,6 +31,11 @@ class WorkManagerActivity : AppCompatActivity() {
         //setting constraints to be met for the work request to run (now the work will run only when phone is charging)
         val constraints = Constraints.Builder()
             .setRequiresCharging(true).build()
+
+        //sending input data to the workmanager
+        val inputData = Data.Builder()
+            .putInt(KEY_INPUT_DATA, 200)
+            .build()
         //making the work request (One time work request)
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .setConstraints(constraints)
@@ -35,6 +46,16 @@ class WorkManagerActivity : AppCompatActivity() {
         //adding a listener (live data observer) to get the status of the work enqueued
         workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this) {
             findViewById<TextView>(R.id.textView).text = it.state.name
+
+            //getting the output data received from the worker class when task is finished
+            if (it.state.isFinished) {
+                Toast.makeText(
+                    applicationContext,
+                    it.outputData.getString(UploadWorker.KEY_OUTPUT_DATA),
+                    Toast.LENGTH_LONG
+                ).show()
+
+            }
         }
     }
 }
